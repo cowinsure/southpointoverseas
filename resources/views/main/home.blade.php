ca8cc54dfcbb248d61b39682a9a06fd4af66fc77
cowinsure <cowinsure@gmail.com>
2025-07-23 12:02:21 +0600

    fix:updated video embedding set

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -408,12 +408,14 @@
         class="video-content-bg"
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/JhpMSJfbtJk"
+        src="https://www.youtube.com/embed/K4TxWzvxoM4"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
     ></iframe>
+
+
     <!--------------------------------------------- video-content-bg end ------------------------------------>
 
     <!-- section start -->
